091036229d0438262378d627d4291898411e66fe
STEPHANIE <cg3022692@labA205m16.IFSP>
2024-10-02 22:11:13 -0300

    init react

---
+++ react-next-frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import { BsArrowThroughHeartFill } from "react-icons/bs";
+
+export default function Header () {
+    return (
+        <div className="flex bg-black h-14">
+            <BsArrowThroughHeartFill className="text-white text-3xl"/>
+        </div>
+    )
+}
